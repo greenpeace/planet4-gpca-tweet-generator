eafc15e0f68eac7684138e1a2ea1963660b81080
Mohammad Rafey <mohammadrafey0@gmail.com>
2022-11-28 14:03:51 -0500

    BUILD: build files

--- build/client.asset.php
+++ build/client.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '6ea6dc7b175a51787227');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'b7f522473e5268dcb4c6');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '16b8429268a89071dffd');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6f274ea4413f9da2d718');

fe8b43d18d90b603ed69b3aa1b16a47bcbcf4677
Mohammad Rafey <mohammadrafey0@gmail.com>
2022-11-28 16:59:29 -0500

    Removed global style of max-width

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6f274ea4413f9da2d718');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '563692c5033229f2766d');

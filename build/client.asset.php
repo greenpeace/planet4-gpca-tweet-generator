ed203ec8b0bb0618a90a5c54cc6cb17d3b8e77ff
Mohammad Rafey <mohammadrafey0@gmail.com>
2022-12-12 14:54:36 -0500

    Plugin now supports dynamic labels

--- build/client.asset.php
+++ build/client.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '591fde508c0c3ec90241');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'ca3ac6a819eccfd3beb6');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3cdf11495de22d489ca4');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd5cb0430b874efcf1cf5');
